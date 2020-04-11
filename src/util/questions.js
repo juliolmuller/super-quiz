c12ce66eb8394c5da4c482cb2726504a32e780d4
Julio Muller <juliodolago@gmail.com>
2020-04-11 12:57:50 -0300

    Linted code

--- src/util/questions.js
+++ src/util/questions.js
@@ -1,56 +1,56 @@
 export default [
-    {
-        text: 'Qual é o coletivo de cães?',
-        answers: [
-            { text: 'MATILHA', correct: true },
-            { text: 'REBANHO', correct: false },
-            { text: 'CARDUME', correct: false },
-            { text: 'MANADA', correct: false }
-        ]
-    },
-    {
-        text: 'Qual é a maior floresta do planeta?',
-        answers: [
-            { text: 'NEGRA', correct: false },
-            { text: 'DE SHERWOOD', correct: false },
-            { text: 'DA TIJUCA', correct: false },
-            { text: 'AMAZÔNICA', correct: true }
-        ]
-    },
-    {
-        text: 'Quem é o primeiro substituto do Presidente?',
-        answers: [
-            { text: 'VEREADOR', correct: false },
-            { text: 'VICE-PRESIDENTE', correct: true },
-            { text: 'SENADOR', correct: false },
-            { text: 'DEPUTADO', correct: false }
-        ]
-    },
-    {
-        text: 'Segundo o romance, que animal era “Moby Dick”?',
-        answers: [
-            { text: 'TUBARÃO', correct: false },
-            { text: 'GOLFINHO', correct: false },
-            { text: 'POLVO', correct: false },
-            { text: 'BALEIA', correct: true }
-        ]
-    },
-    {
-        text: 'Que animal é o Pateta?',
-        answers: [
-            { text: 'CAVALO', correct: false },
-            { text: 'BURRO', correct: false },
-            { text: 'CACHORRO', correct: true },
-            { text: 'RAPOSA', correct: false }
-        ]
-    },
-    {
-        text: 'Qual destes personagens não veste uma capa?',
-        answers: [
-            { text: 'SUPER-HOMEM', correct: false },
-            { text: 'BATMAN', correct: false },
-            { text: 'WOLVERINE', correct: true },
-            { text: 'ZORRO', correct: false }
-        ]
-    },
+  {
+    text: 'Qual é o coletivo de cães?',
+    answers: [
+      { text: 'Matilha', correct: true },
+      { text: 'Rebanho', correct: false },
+      { text: 'Cardume', correct: false },
+      { text: 'Manada', correct: false },
+    ],
+  },
+  {
+    text: 'Qual é a maior floresta do planeta?',
+    answers: [
+      { text: 'Negra', correct: false },
+      { text: 'De Sherwood', correct: false },
+      { text: 'Da Tijuca', correct: false },
+      { text: 'Amazônica', correct: true },
+    ],
+  },
+  {
+    text: 'Quem é o primeiro substituto do Presidente?',
+    answers: [
+      { text: 'Vereador', correct: false },
+      { text: 'Vice-Presidente', correct: true },
+      { text: 'Senador', correct: false },
+      { text: 'Deputado', correct: false },
+    ],
+  },
+  {
+    text: 'Segundo o romance, que animal era “Moby Dick”?',
+    answers: [
+      { text: 'Tubarão', correct: false },
+      { text: 'Golfinho', correct: false },
+      { text: 'Polvo', correct: false },
+      { text: 'Baleia', correct: true },
+    ],
+  },
+  {
+    text: 'Que animal é o Pateta?',
+    answers: [
+      { text: 'Cavalo', correct: false },
+      { text: 'Burro', correct: false },
+      { text: 'Cachorro', correct: true },
+      { text: 'Raposa', correct: false },
+    ],
+  },
+  {
+    text: 'Qual destes personagens não veste uma capa?',
+    answers: [
+      { text: 'Super-Homem', correct: false },
+      { text: 'Batman', correct: false },
+      { text: 'Wolverine', correct: true },
+      { text: 'Zorro', correct: false },
+    ],
+  },
 ]
