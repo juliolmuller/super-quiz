173c81d4bc9ac55b3d722b6e2993216aea5e0fe6
juliolmuller <juliolmuller@outlook.com>
2025-01-24 18:39:13 -0300

    refactor(deps): migrate from `vue-cli` to `vite`

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import vue from '@vitejs/plugin-vue'
+import rollupAnalyze from 'rollup-plugin-analyzer';
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupAnalyze({
+          summaryOnly: true,
+          limit: 10,
+        }),
+      ],
+    },
+  },
+  plugins: [vue()],
+})
